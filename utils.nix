0f458c33cde1f2a8458d443789bcd5238de63f4d
AshesOfEther <15670490+AshesOfEther@users.noreply.github.com>
2024-12-01 23:12:35 +0100

    Add some more reusable utils

--- utils.nix
+++ utils.nix
@@ -1,4 +1,14 @@
-{ lib }: with lib; {
+{ lib }: with lib; rec {
+    abs = x: if x < 0 then -x else x;
+
+    eq = x: y: x == y;
+
+    delta = x: y: abs;
+
+    listSum = foldl' add 0;
+
+    sort' = sort lessThan;
+
     splitLines = s: let
         lines = splitString "\n" s;
         amount = lines;
