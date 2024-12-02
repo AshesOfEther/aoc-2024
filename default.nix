182845965fff401e50860ffc3f148bca7490a1ea
AshesOfEther <15670490+AshesOfEther@users.noreply.github.com>
2024-12-02 11:09:57 +0100

    Remove old unused code from `default.nix`

--- default.nix
+++ default.nix
@@ -2,18 +2,7 @@ let
     pkgs = import <nixpkgs> {};
     lib = pkgs.lib;
     utils = import ./utils.nix { lib = lib; };
-
-    day = ./day1;
-    part1 = import (day + /part1.nix) { utils = utils; };
-    part2 = import (day + /part2.nix) { utils = utils; };
-    input = pkgs.lib.readFile (day + /input.txt);
-
-
 in
-    #lib.listToAttrs (map (num: let name = "day${builtins.toString num}"; in {
-    #    name = name;
-    #    value = import ./${name};
-    #}) (lib.range 1 25))
     lib.pipe (builtins.readDir ./.) [
         (lib.filterAttrs (name: type: lib.hasPrefix "day" name && type == "directory"))
         (lib.mapAttrs (name: _: let
@@ -29,10 +18,3 @@ in
             };
         in { part1 = part 1; part2 = part 2; }))
     ]
-    
-    #part1.solution {
-    #    #input = (builtins.elemAt day1.tests 0).input;
-    #    input = input;
-    #    lib = pkgs.lib;
-    #    utils = utils;
-    #}
